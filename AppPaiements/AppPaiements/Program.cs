cbfc0c069979d014a6e394019f494c8ed793ddf4
nour <300138573@monboreal.ca>
2024-12-07 19:37:29 -0500

    3e Commit pour CarteCredit.cs et Paiement.cs

--- AppPaiements/AppPaiements/Program.cs
+++ AppPaiements/AppPaiements/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using AppPaiements;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CarteCredit paiementCC1 = new CarteCredit(1050.50, "Achat de Pc", 345768120);
+        CarteCredit paiementCC2 = new CarteCredit(1200.40, "Achat de server", 984761876);
+        CarteCredit paiementCC3 = new CarteCredit(345, "Epicerie", 456896333);
+
+        paiementCC1.AfficherDetails();
+        paiementCC2.AfficherDetails();
+        paiementCC3.AfficherDetails();
     }
 }
